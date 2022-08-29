fd0b14b2dde35c74c10242a3ae73a77532791c4d
Andrew <vishnyaishere@gmail.com>
2022-08-29 16:41:17 +0300

    exc007 doen

--- exc007_hw/Program.cs
+++ exc007_hw/Program.cs
@@ -6,3 +6,24 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+int rows = 4;
+int columns = 5;
+
+int[,] array2D = new int[rows, columns];
+
+double[] meanColumns = new double[columns];
+
+Random rand = new Random();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        array2D[i, j] = rand.Next(1, 10);
+        meanColumns[j] += Convert.ToDouble(array2D[i, j]) / rows;
+        Console.Write($" {array2D[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Mean average of each column: " + string.Join("; ", meanColumns.Select(x => Math.Round(x, 1))));
