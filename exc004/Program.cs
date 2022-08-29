8b838f531004b37d88563f44854266bc09c5c46e
Andrew <vishnyaishere@gmail.com>
2022-08-29 15:25:31 +0300

    exc004 done

--- exc004/Program.cs
+++ exc004/Program.cs
@@ -7,3 +7,24 @@
 // Сумма элементов главной диагонали: 1+9+2 = 12
 // Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может быть прямоугольный
 
+int rows = 4;
+int columns = 5;
+
+int[,] array2D = new int[rows, columns];
+
+Random rand = new Random();
+
+int sumDiagonal = 0;
+
+for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+        array2D[i, j] = rand.Next(1, 10);
+        Console.Write($" {array2D[i, j]} ");
+        if (i == j){
+            sumDiagonal += array2D[i, j];
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Sum of elements on main diagonal = {sumDiagonal}");
