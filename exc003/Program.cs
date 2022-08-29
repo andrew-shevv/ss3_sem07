325b98cd4f0d70adfefe99ce3247b59c5d206d83
Andrew <vishnyaishere@gmail.com>
2022-08-29 15:20:34 +0300

    exc003 done

--- exc003/Program.cs
+++ exc003/Program.cs
@@ -11,3 +11,31 @@
 // 5 81 2 9
 // 8 4 2 4
 
+Console.WriteLine("Creating array: ");
+
+int rows = 5;
+int columns = 4;
+
+int[,] array2D = new int[rows, columns];
+
+Random rand = new Random();
+
+for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+        array2D[i, j] = rand.Next(-9, 10);
+        Console.Write($" {array2D[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nNewly altered array: ");
+
+for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+        if (i % 2 == 0 && j % 2 == 0){
+            array2D[i, j] *= array2D[i, j];
+        }
+        Console.Write($" {array2D[i, j]} ");
+    }
+    Console.WriteLine();
+}
