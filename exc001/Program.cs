7d3321fe28fe1fbef5e4ef43b12a30be6f472489
Andrew <vishnyaishere@gmail.com>
2022-08-29 16:08:21 +0300

    exc001 added methods

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -5,17 +5,39 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
-int rows = 5;
-int columns = 4;
+int[,] CreateArray2D(int verticalLength, int horizontalLength, int minValue = -9, int maxValue = 9)
+{
+    int[,] array2D = new int[verticalLength, horizontalLength];
 
-int[,] array2D = new int[rows, columns];
+    Random rand = new Random();
 
-Random rand = new Random();
-
-for (int i = 0; i < rows; i++){
-    for (int j = 0; j < columns; j++){
-        array2D[i, j] = rand.Next(-19, 20);
-        Console.Write($" {array2D[i, j]} ");
+    for (int i = 0; i < verticalLength; i++)
+    {
+        for (int j = 0; j < horizontalLength; j++)
+        {
+            array2D[i, j] = rand.Next(minValue, maxValue + 1);
+        }
     }
-    Console.WriteLine();
+    return array2D;
 }
+
+void PrintArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            Console.Write($" {array2D[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input vertical length: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Input horizontal length: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = CreateArray2D(rows, columns);
+
+PrintArray2D(matrix);
