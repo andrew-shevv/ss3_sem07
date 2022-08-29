61901af45c6d4b4eeef44cbfb1cfd1583dac6fc2
Andrew <vishnyaishere@gmail.com>
2022-08-29 15:07:10 +0300

    exc002 done

--- exc002/Program.cs
+++ exc002/Program.cs
@@ -6,3 +6,15 @@
 // 1 2 3 4
 // 2 3 4 5
 
+int rows = 5;
+int columns = 4;
+
+int[,] array2D = new int[rows, columns];
+
+for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+        array2D[i, j] = i + j;
+        Console.Write($" {array2D[i, j]} ");
+    }
+    Console.WriteLine();
+}
