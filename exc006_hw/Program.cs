3eaee6f4daa0745659ebc00c4a06de6fed13a1e1
Andrew <vishnyaishere@gmail.com>
2022-08-29 16:09:33 +0300

    exc006 done

--- exc006_hw/Program.cs
+++ exc006_hw/Program.cs
@@ -8,3 +8,34 @@
 // 8 4 2 4
 // 1 7 -> такого числа в массиве нет
 
+int rows = 4;
+int columns = 5;
+
+int[,] array2D = new int[rows, columns];
+
+Random rand = new Random();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        array2D[i, j] = rand.Next(1, 10);
+        Console.Write($" {array2D[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input vertical position of element: ");
+int verticalIndex = int.Parse(Console.ReadLine()!);
+Console.Write("Input horizontal position of element: ");
+int horizontalIndex = int.Parse(Console.ReadLine()!);
+
+if (verticalIndex < rows
+    && verticalIndex >= 0
+    && horizontalIndex < columns
+    && horizontalIndex >= 0
+)
+{
+    Console.WriteLine($"{array2D[verticalIndex, horizontalIndex]}");
+}
+else Console.WriteLine("Element doesn't exist in the array");
