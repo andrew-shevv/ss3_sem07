1f7a5061e0a037a33e4bc7d22df03d4ca204ac20
Andrew <vishnyaishere@gmail.com>
2022-08-29 15:03:56 +0300

    exc001 done

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -5,3 +5,17 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
+int rows = 5;
+int columns = 4;
+
+int[,] array2D = new int[rows, columns];
+
+Random rand = new Random();
+
+for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+        array2D[i, j] = rand.Next(-19, 20);
+        Console.Write($" {array2D[i, j]} ");
+    }
+    Console.WriteLine();
+}
